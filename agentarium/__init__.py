adc13633ea7974c616da4ef4da91edffa839c233
Valentin De Matos <vltn.dematos@gmail.com>
2025-01-03 17:42:27 +0100

    !refactor: separate actions from agent and switch pickl to dill
    Signed-off-by: Valentin De Matos <vltn.dematos@gmail.com>

--- agentarium/__init__.py
+++ agentarium/__init__.py
@@ -1,13 +1,13 @@
 from .Agent import Agent
-# from .Environment import Environment
 from .AgentInteractionManager import AgentInteractionManager
+from .actions.Action import Action
 from .Interaction import Interaction
 
 __version__ = "0.2.8"
 
 __all__ = [
     "Agent",
-    "Environment",
     "AgentInteractionManager",
-    "Interaction"
+    "Interaction",
+    "Action",
 ]
