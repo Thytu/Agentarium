e74262e2f2361ea0f98b3abaa5524a0fb825a1b8
Valentin De Matos <vltn.dematos@gmail.com>
2025-01-03 17:55:03 +0100

    chore: bump version to 0.3.0
    Signed-off-by: Valentin De Matos <vltn.dematos@gmail.com>

--- agentarium/__init__.py
+++ agentarium/__init__.py
@@ -3,7 +3,7 @@ from .AgentInteractionManager import AgentInteractionManager
 from .actions.Action import Action
 from .Interaction import Interaction
 
-__version__ = "0.2.8"
+__version__ = "0.3.0"
 
 __all__ = [
     "Agent",
